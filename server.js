1f8599d2e166a3309141f67edd4cfdae1d83ead7
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-06 10:27:43 +0200

    Re coded the routes

--- server.js
+++ server.js
@@ -18,22 +18,21 @@ const users = [
     }
 ]
 
-// Fetching user by id
+// Fetching users by id
 app.get('/api/users/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const user = users.find((user) => user.id === id);
-
     if(user){
-        return res.status(200).json(user);
-    }res.status(404).json({msg: `User of id ${id} not found.`})
+        return res.status(200).json(user)
+    }res.status(404).json({msg: `User of id: ${id} not found`});
 })
 
-//Fetching users and with limits
-app.get('/api/users', (req, res) => {
+// Fetching users and with their limit
+app/get('/api/users', (req, res) => {
     const lim = parseInt(req.query.limit);
     if(lim > 0){
-        return res.status(200).json(users.slice(0, lim))
-    } res.status(200).json(users)
+        return res.json(users.slice(0, lim));
+    } res.json(users)
 })
 
 // Running the server
