bbec289de7f473b456253f775e0dd7754eb54f17
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-07 14:38:59 +0200

    Logger middleware

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from "express";
 const app = express();
 import users from './routes/users.js';
+import logger from './middleware/logger.js';
 const PORT = 3000;
 
 // Parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
 
 // Routes
 app.use('/api/users', users);
