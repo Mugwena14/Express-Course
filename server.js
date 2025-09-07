3db58ff72ef6c6d3d517f605467a73beb41339f3
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-07 10:35:22 +0200

    Added the parser middleware

--- server.js
+++ server.js
@@ -6,5 +6,9 @@ const PORT = 3000;
 // Routes
 app.use('/api/users', users);
 
+// Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // Running the server
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
