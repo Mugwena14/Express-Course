4676350656ff67376ab9c69000c8e0611e5b3777
Langavi Makhubele <mlangaviclyde@gmail.com>
2025-09-09 14:54:12 +0200

    Get users controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,16 +7,3 @@ const getUser = (req, res) => {
         return res.status(200).json(newUsers);
     } res.status(200).json(users)
 }
-
-// @desc Getting user by id
-// @routes GET /api/users/:id
-const getUserById = (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const user = users.find((user) => user.id === id);
-
-    if(!user){
-        const error = new Error(`User of the id: ${id} not found.`)
-        error.status = 404;
-        return next(error);
-    } res.status(200).json(user);
-}
--- routes/users.js
+++ routes/users.js
@@ -14,51 +14,13 @@ router.get('/', )
 router.get('/:id', )
 
 // Adding new user
-router.post('/', (req, res, next) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name
-    }
-
-    if(!newUser.name){
-        const error = new Error(`Please enter your name`);
-        error.status = 400;
-        return next(error);
-    } users.push(newUser);
-    res.status(201).json(users);
-});
+router.post('/', );
 
 // Updating user
-router.put('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const user = users.find((user) => user.id === id);
-
-    if(!user){
-        const error = new Error(`User of id: ${id} not found.`);
-        error.status = 404;
-        return next(error);
-    }
-
-    user.name = req.body.name;
-    if(!user.name){
-        const error = new Error(`Please enter your name`);
-        error.status = 400;
-        return next(error);
-    } res.status(200).json(users);
-})
+router.put('/:id', )
 
 // Deleting users
-router.delete('/:id', (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const newUsers = users.filter((user) => user.id !== id)
-    const user = users.find((user) => user.id === id);
-
-    if(!user){
-        const error = new Error(`User of the id: ${id} not found.`);
-        error.status = 404;
-        return next(error);
-    } res.status(200).json(newUsers);
-})
+router.delete('/:id', )
 
 
 
